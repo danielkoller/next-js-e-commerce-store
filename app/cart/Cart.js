bf9897bfe856f4354db6e5c595a67028e731aa36
Daniel Koller <daniel-koller@gmx.at>
2023-02-10 09:54:41 +0100

    Add working Cart

--- app/cart/Cart.js
+++ app/cart/Cart.js
@@ -1,12 +1,47 @@
 import { cookies } from 'next/headers';
 
 export function Cart({ bikes }) {
-  const bikesInCookies = cookies().get('cart');
+  const bikesCookie = cookies().get('cart');
 
-  let bikesInCookiesParsed = [];
+  let bikesCookieParsed = [];
 
-  if (bikesInCookies) {
-    bikesInCookiesParsed = JSON.parse(bikesInCookies.value);
-    console.log(bikesInCookiesParsed);
+  if (bikesCookie) {
+    bikesCookieParsed = JSON.parse(bikesCookie.value);
   }
+
+  let bikesInCart = [];
+
+  bikesInCart = bikes.map((bike) => {
+    const bikeInCart = { ...bike, amount: 0 };
+
+    const bikeInCookie = bikesCookieParsed.find(
+      (bikeObject) => bike.id === bikeObject.id,
+    );
+
+    if (bikeInCookie) {
+      bikeInCart.amount = bikeInCookie.amount;
+    }
+
+    return bikeInCart;
+  });
+
+  let total = 0;
+  bikesInCart.forEach((bike) => {
+    total += bike.price * bike.amount;
+  });
+
+  return (
+    <div>
+      <ul>
+        {bikesInCart
+          .filter((bike) => bike.amount > 0)
+          .map((bike) => (
+            <li key={bike.id}>
+              {bike.name} ({bike.amount})
+            </li>
+          ))}
+      </ul>
+      <p>Total: {total} €</p>
+    </div>
+  );
 }
